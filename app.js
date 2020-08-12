d4ea0f1d5a074b6c6849f70b033a2c652f1fe7f5
Danny Peck <dannytpeck@gmail.com>
2020-08-12 13:56:04 -0700

    Add logic to strip out unwanted text in title, instructions, and more info

--- app.js
+++ app.js
@@ -4,9 +4,33 @@ const url = `https://api.airtable.com/v0/appa7mnDuYdgwx2zP/Challenges/${id}?api_
 
 $.getJSON(url).done(data => {
   const image = data.fields['Header Image'];
-  const title = data.fields['Title'];
-  const instructions = data.fields['Instructions'];
-  const moreInformation = data.fields['More Information Html'];
+
+  // Remove the year from the title, if present
+  const title = data.fields['Title']
+    .replace(/2017: /, '')
+    .replace(/2018: /, '');
+
+  // Strip out old branding bits (YOUR CHALLENGE, forced styles)
+  const instructions = data.fields['Instructions']
+    .replace(/YOUR CHALLENGE: /, '')
+    .replace(/ style="font-weight: bold; font-size: 14px;*"/, '')
+    .replace(/ style="font-size:14px; font-weight:bold"/, '')
+    .replace(/<\/*strong>/g, '');
+
+  const moreInformation = data.fields['More Information Html']
+    // Remove FOCUS ON box and quote box
+    .replace(/<div class="brandingBckgrndColor" style="margin-bottom:9px;padding:10px;color:white;">.*<\/p><\/div>/s, '')
+    .replace(/<div style="margin-bottom: 9px; padding: 10px; color: white;".*<\/div>/s, '')
+    .replace(/<div style="margin-bottom: 9px;.*FOCUS ON.*<\/h4><\/div>/s, '')
+    .replace(/<div style="margin-bottom: 9px.*IGNITE YOUR LIFE<\/h4><\/div>/s, '')
+    // Remove Tagline
+    .replace(/<h3.*class="brandingBckgrndColor".*<\/h3>/, '')
+    // Remove coaching
+    .replace(/<span class="coachinginfo">.*<\/span>/s, '')
+    // Remove bottom image
+    .replace(/<img.*_web.*>/, '')
+    // Remove all caps copyright
+    .replace(/<p style="font-size: .*Copyright.*ADURO.*<\/p>/, '')
 
   $('#challenge-container').html(`
     <div id="image-wrapper">
